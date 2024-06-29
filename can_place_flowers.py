a57b896b7b787e1950a1f399e8f1ebc55ad2af18
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-06-29 10:39:23 -0700

    solution

--- can_place_flowers.py
+++ can_place_flowers.py
@@ -22,3 +22,12 @@ flowerbed[i] is 0 or 1.
 There are no two adjacent flowers in flowerbed.
 0 <= n <= flowerbed.length
 '''
+
+def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        f = [0] + flowerbed + [0]
+        for i in range(1, len(f)-1):
+            if f[i] == 0 and f[i + 1 ]== 0 and f[i -1 ]== 0:
+                f[i] = 1
+                n-=1
+        return n <= 0
+
